3d82224db02ff939be6eb6250d6e60e45365d507
kymaka <seafth345@gmail.com>
2022-09-08 19:21:12 +0700

    Added initialize method to pass Pane in different controllers.

--- Diagrams/src/main/java/gui/controllers/EntityController.java
+++ Diagrams/src/main/java/gui/controllers/EntityController.java
@@ -1,31 +1,36 @@
 package gui.controllers;
 
 import gui.shapes.CustomRectangle;
+import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class EntityController {
 
   @FXML
-  private Pane pane;
+  DrawController drawController;
 
   @FXML
-  protected void onDrawButtonClick() {
-    CustomRectangle rect = new CustomRectangle(100, 100, 100, 200);
-    rect.setFill(Color.WHITE);
-    DragController dragController = new DragController(rect, true);
-    TextField textField = new TextField();
-    textField.setText("Some text");
-    pane.getChildren().addAll(rect, rect.getChildren().get(0), textField);
-  }
+  ToolbarController toolbarController;
 
-  public Node createShape() {
-    CustomRectangle rect = new CustomRectangle(100, 100, 50, 200);
-    DragController dragController = new DragController(rect, true);
-    return rect;
+  @FXML
+  public Pane pane;
+  @FXML
+  public Button draw;
+  @FXML
+  public ToolBar toolbar;
+
+  @FXML
+  public void initialize(){
+    drawController.setPane(this.pane);
+    toolbarController.setPane(this.pane);
   }
 
 }
