e79d996776218f9d6ed6d659a14187a59192b2cc
kymaka <seafth345@gmail.com>
2022-09-08 19:19:13 +0700

    Controller testing "draw" button.

---
+++ Diagrams/src/main/java/gui/controllers/DrawController.java
@@ -0,0 +1,36 @@
+package gui.controllers;
+
+import gui.shapes.CustomRectangle;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+public class DrawController {
+
+  @FXML
+  public Button draw;
+
+  public Pane pane;
+
+  public DrawController() {
+  }
+  public void setPane(Pane pane) {
+    this.pane = pane;
+
+  }
+
+  @FXML
+  protected void onDrawButtonClick() {
+
+    CustomRectangle rect = new CustomRectangle(100, 100, 100, 200);
+    rect.setFill(Color.WHITE);
+    DragController dragController = new DragController(rect, true);
+
+    pane.getChildren().add(rect);
+    for (Node n : rect.getChildren())
+      pane.getChildren().add(n);
+  }
+}
