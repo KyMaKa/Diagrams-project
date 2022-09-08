f298decd5c3819e7b04345ebdf58fdc7af359c37
kymaka <seafth345@gmail.com>
2022-09-08 19:22:14 +0700

    trash

---
+++ Diagrams/src/main/java/gui/controllers/LoadController.java
@@ -0,0 +1,16 @@
+package gui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoadController {
+
+  @FXML
+  private Button load;
+
+  @FXML
+  protected void onLoadButtonClick() {
+
+  }
+
+}
