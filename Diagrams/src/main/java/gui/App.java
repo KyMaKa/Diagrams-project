f2f1568c47c1a99e278cf9d1c25d128c02a60cbd
kymaka <seafth345@gmail.com>
2022-09-08 19:22:05 +0700

    App.

--- Diagrams/src/main/java/gui/App.java
+++ Diagrams/src/main/java/gui/App.java
@@ -1,14 +1,20 @@
 package gui;
 
 import gui.controllers.DragController;
+import gui.controllers.EntityController;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import com.google.gson.Gson;
 
 import java.io.IOException;
+import logic.Entity.Entity;
 
 public class App extends Application {
 
@@ -16,15 +22,16 @@ public class App extends Application {
 
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/gui/hello-view.fxml"));
+    //fxmlLoader.load();
     Scene scene = new Scene(fxmlLoader.load());
-    scene.getStylesheets().add(getClass().getResource("style.css").toString());
+    scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toString());
     stage.setTitle("UML diagrams");
     stage.setScene(scene);
     stage.show();
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     launch();
   }
 }
