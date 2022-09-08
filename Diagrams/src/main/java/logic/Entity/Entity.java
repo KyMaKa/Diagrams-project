41876babc866bdc0212d5379e6e049ab495a9325
kymaka <seafth345@gmail.com>
2022-09-08 19:21:24 +0700

    Added coordinates.

--- Diagrams/src/main/java/logic/Entity/Entity.java
+++ Diagrams/src/main/java/logic/Entity/Entity.java
@@ -15,6 +15,26 @@ public class Entity {
 
   private Relationship[] relations;
 
+  private double posX;
+
+  private double posY;
+
+  public double getPosX() {
+    return posX;
+  }
+
+  public void setPosX(double posX) {
+    this.posX = posX;
+  }
+
+  public double getPosY() {
+    return posY;
+  }
+
+  public void setPosY(double posY) {
+    this.posY = posY;
+  }
+
   public String getName() {
     return name;
   }
