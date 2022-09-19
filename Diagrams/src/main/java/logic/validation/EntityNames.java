75a8d6e914a5003e9533b7822474a60656248801
kymaka <seafth345@gmail.com>
2022-09-20 00:02:26 +0700

    Stores all entity names.

---
+++ Diagrams/src/main/java/logic/validation/EntityNames.java
@@ -0,0 +1,10 @@
+package logic.validation;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class EntityNames {
+  public static Map<String, Integer> entityNames = new HashMap<>();
+}
