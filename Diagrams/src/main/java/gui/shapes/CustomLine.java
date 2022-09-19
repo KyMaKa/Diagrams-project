9ff1cbb378f0306434e677f0a2430c6a2c2245b3
kymaka <seafth345@gmail.com>
2022-09-19 21:19:42 +0700

    Added type selector.

--- Diagrams/src/main/java/gui/shapes/CustomLine.java
+++ Diagrams/src/main/java/gui/shapes/CustomLine.java
@@ -1,15 +1,26 @@
 package gui.shapes;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
+import logic.Types.RelTypes;
 
 public class CustomLine extends Line {
 
-  private final Text parentText = new Text("0..1");
+  private Line arrow1;
 
-  private final Text childText = new Text("0..1");
+  private Line arrow2;
 
+  private final ChoiceBox<RelTypes> relType = new ChoiceBox<>();
 
   private CustomRectangle child;
 
@@ -33,18 +44,18 @@ public class CustomLine extends Line {
   }
 
   public CustomLine() {
-    this.parentText.layoutXProperty().bind(this.startXProperty().add(10));
-    this.parentText.layoutYProperty().bind(this.startYProperty());
-    this.childText.layoutXProperty().bind(this.endXProperty().add(10));
-    this.childText.layoutYProperty().bind(this.endYProperty());
+    this.relType.getItems().addAll(RelTypes.values());
+    this.relType.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, CornerRadii.EMPTY, Insets.EMPTY)));
+    this.relType.layoutXProperty().bind(this.startXProperty().add(10));
+    this.relType.layoutYProperty().bind(this.startYProperty());
   }
 
-  public Text getParentText() {
-    return this.parentText;
+  public RelTypes getRelType() {
+    return this.relType.getValue();
   }
 
-  public Text getChildText() {
-    return this.childText;
+  public void setRelType(RelTypes type) {
+    this.relType.setValue(type);
   }
 
   public void setLine(Node source, Node connector, Node tSource, Node target) {
@@ -56,6 +67,67 @@ public class CustomLine extends Line {
     this.startYProperty().bind(source.layoutYProperty().add(connector.getLayoutY()));
     this.endXProperty().bind(tSource.layoutXProperty().add(target.getLayoutX()));
     this.endYProperty().bind(tSource.layoutYProperty().add(target.getLayoutY()));
+
+    drawArrowLine();
   }
 
+  private void drawArrowLine() {
+    arrow1 = new Line();
+    arrow2 = new Line();
+    InvalidationListener updater = o -> {
+      double ex = this.getEndX();
+      double ey = this.getEndY();
+      double sx = this.getStartX();
+      double sy = this.getStartY();
+
+      arrow1.setEndX(ex);
+      arrow1.setEndY(ey);
+      arrow2.setEndX(ex);
+      arrow2.setEndY(ey);
+
+      if (ex == sx && ey == sy) {
+        // arrow parts of length 0
+        arrow1.setStartX(ex);
+        arrow1.setStartY(ey);
+        arrow2.setStartX(ex);
+        arrow2.setStartY(ey);
+      } else {
+        double hypot = Math.hypot(sx - ex, sy - ey);
+        double factor = 15 / hypot;
+        double factorO = 7 / hypot;
+
+        // part in direction of main line
+        double dx = (sx - ex) * factor;
+        double dy = (sy - ey) * factor;
+
+        // part orthogonal to main line
+        double ox = (sx - ex) * factorO;
+        double oy = (sy - ey) * factorO;
+
+        arrow1.setStartX(ex + dx - oy);
+        arrow1.setStartY(ey + dy + ox);
+        arrow2.setStartX(ex + dx + oy);
+        arrow2.setStartY(ey + dy - ox);
+      }
+    };
+
+    // add updater to properties
+    startXProperty().addListener(updater);
+    startYProperty().addListener(updater);
+    endXProperty().addListener(updater);
+    endYProperty().addListener(updater);
+    updater.invalidated(null);
+  }
+
+  public ChoiceBox<RelTypes> getRels() {
+    return this.relType;
+  }
+
+  public Line getArrow1() {
+    return arrow1;
+  }
+
+  public Line getArrow2() {
+    return arrow2;
+  }
 }
