8c876f8d1d62d5c3a1d6b6a0af2889119c653c16
kymaka <seafth345@gmail.com>
2022-09-21 03:31:55 +0700

    Deletion for entity and corresponding relations.

--- Diagrams/src/main/java/gui/controllers/DrawController.java
+++ Diagrams/src/main/java/gui/controllers/DrawController.java
@@ -3,11 +3,16 @@ package gui.controllers;
 import gui.shapes.CustomRectangle;
 import java.util.LinkedList;
 import java.util.List;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -18,7 +23,7 @@ import gui.shapes.Connector;
 import gui.shapes.CustomLine;
 import logic.types.RelTypes;
 import logic.validation.EntityNames;
-import logic.validation.PKvalidator;
+import logic.validation.AnnotationValidator;
 
 public class DrawController {
 
@@ -32,16 +37,17 @@ public class DrawController {
 
   private final List<CustomLine> lines = new LinkedList<>();
 
-  private final EventHandler<MouseEvent> handler = mouseEvent -> {
+  private final AnnotationValidator validator = new AnnotationValidator();
+  private final EventHandler<MouseEvent> relationHandler = mouseEvent -> {
+
     Node target = (Node) mouseEvent.getTarget();
-    PKvalidator pKvalidator = new PKvalidator();
     if (source == null) {
       connector = target;
       source = (CustomRectangle) target.getParent();
       System.out.println("Clicked");
     } else {
       Node tSource = target.getParent();
-      if (pKvalidator.validate(source, (CustomRectangle) tSource)) {
+      if (validator.validatePK(source, (CustomRectangle) tSource)) {
         drawRelation(source, (Connector) connector, (CustomRectangle) tSource, (Connector) target,
             null);
       } else {
@@ -51,6 +57,29 @@ public class DrawController {
       connector = null;
     }
   };
+
+  private final EventHandler<KeyEvent> deleteHandler = event -> {
+    if (event.getCode() == KeyCode.DELETE) {
+      CustomRectangle target = (CustomRectangle) event.getSource();
+      removeLine(target);
+      for (CustomLine c : target.getParents()) {
+        removeLine(c.getParentRect());
+        this.pane.getChildren().remove(target);
+      }
+      this.pane.getChildren().remove(target);
+    }
+  };
+
+  private void removeLine(CustomRectangle target) {
+    target.getRelations().forEach((key, value) -> {
+      for (CustomLine c : value) {
+        this.pane.getChildren().remove(c.getArrow1());
+        this.pane.getChildren().remove(c.getArrow2());
+        this.pane.getChildren().remove(c.getRels());
+        this.pane.getChildren().remove(c);
+      }
+    });
+  }
 
   public DrawController() {
   }
@@ -70,7 +99,8 @@ public class DrawController {
 
   public void drawEntity(CustomRectangle rect) {
     new DragController(rect, true);
-    rect.setConnectorPressed(handler);
+    rect.setConnectorPressed(relationHandler);
+    rect.setDeleteEvent(deleteHandler);
     if (EntityNames.entityNames.containsKey(rect.getName())) {
       String newName = rect.getName() + EntityNames.entityNames.get(rect.getName());
       EntityNames.entityNames.put(rect.getName(), EntityNames.entityNames.get(rect.getName()) + 1);
@@ -83,11 +113,22 @@ public class DrawController {
 
   public void drawRelation(CustomRectangle source, Connector connector, CustomRectangle tSource, Connector target, RelTypes type) {
     CustomLine customLine = new CustomLine();
+    ChoiceBox<RelTypes> relType = customLine.getRels();
+    relType.setOnAction(event -> {
+      if (relType.getValue().equals(RelTypes.OneToMany) || relType.getValue().equals(RelTypes.ManyToMany)) {
+        if (!validator.validateFK(source, tSource)) {
+          relType.setValue(RelTypes.OneToOne);
+        }
+      }
+    });
+
     customLine.setLine(source, connector, tSource, target);
     customLine.setChild(tSource);
     customLine.setChildConnector(target);
     customLine.setRelType(type);
     source.addConnector(connector, customLine);
+    customLine.setParentRect(source);
+    tSource.addParent(customLine);
     this.pane.getChildren().addAll(customLine, customLine.getRels(), customLine.getArrow2(), customLine.getArrow1());
   }
 }
