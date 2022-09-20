2b8c724aab1be5da216fd47d8b421e91fe192b10
kymaka <seafth345@gmail.com>
2022-09-21 03:30:09 +0700

    Added toString method and FK annotation.

--- Diagrams/src/main/java/logic/types/Annotations.java
+++ Diagrams/src/main/java/logic/types/Annotations.java
@@ -1,7 +1,20 @@
 package logic.types;
 
 public enum Annotations {
-  NOT_NULL,
-  PK,
+  NOT_NULL {
+    public String toString() {
+      return "NOT NULL";
+    }
+  },
+  PK {
+    public String toString() {
+      return "PRIMARY KEY";
+    }
+  },
+  FK {
+    public String toString() {
+      return "FOREIGN KEY";
+    }
+  },
   UNIQUE,
 }
